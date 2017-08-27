e0d10571b5f7f4e0c2f6790cb1f8d6533751b8d5
Hythem Sidky <sidky@outlook.com>
2017-08-27 03:15:20 -0400

    Added initial rmsd metric.

--- dmaps/distance_matrix.cpp
+++ dmaps/distance_matrix.cpp
@@ -1,24 +1,65 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+#include <cmath>
+#include <iostream>
 #include "distance_matrix.h"
 
 namespace py = pybind11;
+using namespace Eigen;
 
 namespace dmaps
 {
+	f_type rmsd(vector_t ri, vector_t rj, const vector_t& w)
+	{
+		// Maps for ease of use.
+		Map<matrix3_t> xi(ri.data(), ri.size()/3, 3);
+		Map<matrix3_t> xj(rj.data(), rj.size()/3, 3);
+
+		// Subtract out centers of mass.
+		f_type wtot = w.sum();
+		vector3_t comi = (w.asDiagonal()*xi).colwise().sum()/wtot;
+		vector3_t comj = (w.asDiagonal()*xj).colwise().sum()/wtot;
+		xi.rowwise() -= comi.transpose(); 
+		xj.rowwise() -= comj.transpose();
+
+		// SVD of covariance matrix.
+		matrix_t cov = xi.transpose()*w.asDiagonal()*xj;
+		JacobiSVD<matrix_t> svd(cov, ComputeThinU | ComputeThinV);
+		
+		// Find rotation. 
+		f_type d = (svd.matrixV()*svd.matrixU().transpose()).determinant() > 0 ? 1 : -1; 
+		
+		matrix33_t eye = matrix33_t::Identity(3, 3);
+		eye(2, 2) = d;
+		matrix33_t R = svd.matrixV()*eye*svd.matrixU().transpose();
+		
+		// Return rmsd.
+		return sqrt((w.asDiagonal()*(xi - xj*R).array().square().matrix()).sum()/wtot);
+	}
+
 	const matrix_t& distance_matrix::get_coordinates()
 	{
 		return x_;
+	}
+
+	const matrix_t& distance_matrix::get_distances()
+	{
+		return d_;
 	}
 
 	void distance_matrix::compute()
 	{
 		d_ = matrix_t::Zero(x_.rows(), x_.rows());
 		for(int i = 0; i < d_.rows() - 1; ++i)
-			for(int j = i+1; j < d_.rows(); ++j)
+		{
+			for(int j = i + 1; j < d_.rows(); ++j)
 			{
-				
+				d_(i,j) = rmsd(x_.row(i), x_.row(j), w_);
+				d_(j,i) = d_(i,j);
 			}
+		}
 	}
 }
 
@@ -26,5 +67,8 @@ PYBIND11_MODULE(dmaps, m)
 {
 	py::class_<dmaps::distance_matrix>(m, "DistanceMatrix")
 		.def(py::init<dmaps::matrix_t>())
-		.def("get_coordinates", &dmaps::distance_matrix::get_coordinates);
+		.def(py::init<dmaps::matrix_t, dmaps::vector_t>())
+		.def("get_coordinates", &dmaps::distance_matrix::get_coordinates)
+		.def("get_distances", &dmaps::distance_matrix::get_distances)
+		.def("compute", &dmaps::distance_matrix::compute);
 }
--- dmaps/distance_matrix.h
+++ dmaps/distance_matrix.h
@@ -2,12 +2,16 @@
 
 #define f_type double
 
+#include <stdexcept>
 #include <Eigen/Core>
 
 namespace dmaps
 {
-	using matrix_t = Eigen::Matrix<f_type, Eigen::Dynamic, Eigen::Dynamic>; 
-	using matrix3_t = Eigen::Matrix<f_type, Eigen::Dynamic, 3>;
+	using matrix_t = Eigen::Matrix<f_type, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>; 
+	using vector_t = Eigen::Matrix<f_type, Eigen::Dynamic, 1>;
+	using vector3_t = Eigen::Matrix<f_type, 3, 1>;
+	using matrix3_t = Eigen::Matrix<f_type, Eigen::Dynamic, 3, Eigen::RowMajor>;
+	using matrix33_t = Eigen::Matrix<f_type, 3, 3>;
 	
 	class distance_matrix
 	{
@@ -15,15 +19,29 @@ namespace dmaps
 		// Distance matrix.
 		matrix_t d_;
 
+		// Weights vector. 
+		vector_t w_;
+
 		// Coordinates.
 		matrix_t x_;
 	
 	public:
 		distance_matrix(const matrix_t& x) : 
 		x_(x)
-		{}
-		
+		{
+			w_ = vector_t::Ones(x_.cols()/3);
+		}
+
+		distance_matrix(const matrix_t& x, const vector_t& w) : 
+		x_(x), w_(w)
+		{
+			if(x_.cols()/3 != w.size())
+				throw std::runtime_error("Length of weights vector must match dimension of coordinates.");
+		}
+				
 		const matrix_t& get_coordinates(); 
+
+		const matrix_t& get_distances();
 
 		void compute();
 	};
